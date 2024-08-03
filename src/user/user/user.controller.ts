8d07a30c537a4a2c78f26f9867585b544593bc0d
Audyari Wiyono <audy123ari@gmail.com>
2024-08-03 09:32:29 +0700

    5. HTTP Method Decorator

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+  @Post('')
+  createUser(): string {
+    return 'create user';
+  }
+
+  @Get('sample')
+  getUser(): string {
+    return 'Hello NestJS';
+  }
+}
